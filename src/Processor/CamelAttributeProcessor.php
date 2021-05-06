0fdebdc27f366e0566ab37729bc352ef8077744c
k.yamasaki <k.yamasaki@bunnyhop.jp>
2021-05-06 15:01:27 +0900

    fix: リレーションがうまく取得できない問題を修正

--- src/Processor/CamelAttributeProcessor.php
+++ src/Processor/CamelAttributeProcessor.php
@@ -2,9 +2,13 @@
 
 namespace Krlove\EloquentModelGenerator\Processor;
 
+use Illuminate\Support\Str;
+use Illuminate\Database\DatabaseManager;
 use Krlove\CodeGenerator\Model\ArgumentModel;
-use Krlove\CodeGenerator\Model\MethodModel;
 use Krlove\CodeGenerator\Model\DocBlockModel;
+use Krlove\CodeGenerator\Model\MethodModel;
+use Krlove\CodeGenerator\Model\PropertyModel;
+use Krlove\CodeGenerator\Model\UseClassModel;
 use Krlove\EloquentModelGenerator\Config;
 use Krlove\EloquentModelGenerator\Helper\EmgHelper;
 use Krlove\EloquentModelGenerator\Model\EloquentModel;
@@ -16,16 +20,23 @@ use Krlove\EloquentModelGenerator\Model\EloquentModel;
 class CamelAttributeProcessor implements ProcessorInterface
 {
     /**
+     * @var DatabaseManager
+     */
+    protected $databaseManager;
+
+    /**
      * @var EmgHelper
      */
     protected $helper;
 
     /**
      * CamelAttributeProcessor constructor.
+     * @param DatabaseManager $databaseManager
      * @param EmgHelper $helper
      */
-    public function __construct(EmgHelper $helper)
+    public function __construct(DatabaseManager $databaseManager, EmgHelper $helper)
     {
+        $this->databaseManager = $databaseManager;
         $this->helper = $helper;
     }
 
@@ -34,20 +45,39 @@ class CamelAttributeProcessor implements ProcessorInterface
      */
     public function process(EloquentModel $model, Config $config)
     {
+        $schemaManager = $this->databaseManager->connection($config->get('connection'))->getDoctrineSchemaManager();
+        $prefix        = $this->databaseManager->connection($config->get('connection'))->getTablePrefix();
+        $tableDetails       = $schemaManager->listTableDetails($prefix . $model->getTableName());
+
+        $columnNames = [];
+        foreach ($tableDetails->getColumns() as $column) {
+            $columnNames[] = Str::camel($column->getName());
+        }
+        // dd($columnNames);
+        // exit();
+
+        $model->addUses(new UseClassModel('Illuminate\Support\Str'));
+
+        $columnsProperty = new PropertyModel('columns');
+        $columnsProperty->setAccess('private')
+            ->setValue($columnNames)
+            ->setDocBlock(new DocBlockModel('Laravel非標準のカラムリスト', '@var string[]'));
+        $model->addProperty($columnsProperty);
+
         $method = new MethodModel('getAttribute');
         $method->addArgument(new ArgumentModel('key'));
-        $method->setBody('return parent::getAttribute(\Illuminate\Support\Str::snake($key));');
+        $method->setBody('return parent::getAttribute(in_array($key, $this->columns) ? Str::snake($key) : $key);');
         $model->addMethod($method);
 
         $method = new MethodModel('setAttribute');
         $method->addArgument(new ArgumentModel('key'));
         $method->addArgument(new ArgumentModel('value'));
-        $method->setBody('return parent::setAttribute(\Illuminate\Support\Str::snake($key), $value);');
+        $method->setBody('return parent::setAttribute(in_array($key, $this->columns) ? Str::snake($key) : $key, $value);');
         $model->addMethod($method);
 
         $method = new MethodModel('toArray');
         $method
-            ->setBody('$parent = parent::toArray(); $ret = []; foreach($parent as $key => $value) { $ret[\Illuminate\Support\Str::camel((string)$key)] = $value; }; return $ret;')
+            ->setBody('$parent = parent::toArray(); $ret = []; foreach($parent as $key => $value) { $ret[Str::camel((string)$key)] = $value; }; return $ret;')
             ->setDocBlock(new DocBlockModel('@return array<mixed>'));
         $model->addMethod($method);
     }
@@ -57,6 +87,6 @@ class CamelAttributeProcessor implements ProcessorInterface
      */
     public function getPriority()
     {
-        return 11;
+        return 5;
     }
 }
